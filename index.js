629b149c24dcb742107553810a4934af52579af5
tripledawg <iamlesliecrouch>
2022-01-04 18:23:00 -0500

    created index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs'); 
+
+const prompts = [
+    {
+        type: 'input',
+        name: 'title', 
+        message: 'What title would you like to give your project?',
+        validate: 
+    }
+]
