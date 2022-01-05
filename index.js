3cf7eb7d6f1a4992ea1498e0d7e9243e9dd6f365
tripledawg <iamlesliecrouch>
2022-01-05 15:04:59 -0500

    added generateMarkdown.js and package.json; updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const fs = require('fs'); 
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+//const questions = [];
+
+// TODO: Create a function to write README file
+//function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+//function init() {}
+
+// Function call to initialize app
+//init();
+
 const inquirer = require('inquirer');
+
+const fs = require('fs'); 
+
+const generateMarkdown = require('./generateMarkdown');
 
 inquirer
     .prompt([
@@ -44,11 +61,31 @@ inquirer
     {
         type: 'input',
         name: 'tests',
-        message: 'How can this prohect be tested?',
+        message: 'How can this project be tested?',
     },
     {
         type: 'input',
         name: 'questions',
         message: 'Where can users send questions?',
     }
-])
+]);
+const questions = [inquirer.prompt]; //???
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=> {
+        if (err)
+        throw(err); 
+        console.log ('Success! Your README is complete!')
+    });
+};
+
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
+
+init();
