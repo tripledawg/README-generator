384495e92cd2002dd17f492abc5593c315bc3e57
tripledawg <iamlesliecrouch>
2022-01-05 12:57:08 -0500

    began inquirer prompts for README

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const fs = require('fs'); 
+const inquirer = require('inquirer');
 
-const prompts = [
+inquirer
+    .prompt([
     {
         type: 'input',
         name: 'title', 
         message: 'What title would you like to give your project?',
-        validate: 
-    }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How would you like to describe your project?',
+    },
+    {
+        type: 'input',
+        name: 'table of contents',
+        message: 'What would you like to include in your table of contents?'
+        //use concatenation and \n new line command to make it, maybe use a foreach?
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What installation directions you would like to provide your user?',
+    },
+}
 ]
